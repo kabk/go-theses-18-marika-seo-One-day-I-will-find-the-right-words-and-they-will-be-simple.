d7695005f5b2cf77014f9a7fa85427c587403a6e
Marika <marika.l.seo@gmail.com>
2018-01-26 17:25:48 +0100

    yeeee

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// $("p1").lettering();
-
-// var time = 0;
-// setInterval(function(){
-// 	time+=0.3;
-// 	 // $(".wordworld").css("transform", "rotate("+time+"deg)");
-
-// },30)
-
-
-
 for (var i = 0; i < 57; i++) {
 	 $(".char"+(i+1)).css("transform", "rotate("+(i*6.2)+"deg)");
 }
